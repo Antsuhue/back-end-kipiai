eecbdfce624a513edf9803dbb0fde4eddf73203f
antsuhue <anderson_julio_15@hotmail.com>
2021-10-08 09:52:00 -0700

    Iniciando servidor back

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
 const express = require("express")
 const mongoose = require("mongoose")
+const routes = require("./src/routes")
+const cors = require("cors")
 require("dotenv/config")
 
+const app = express()
 const PORT = process.env.PORT
 
-const app = express()
 
-
-
-
-app.get("/", (req,res) => {
-    res.send("Hello World")
-})
+app.use(cors())
+app.use(routes)
 
 app.listen(PORT, ()=> {
     console.log(`Servidor iniciado em: http://localhost:${PORT}`);
